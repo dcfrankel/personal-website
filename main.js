73e42c44beb3a2b55cfb3d0a02411c2964171f67
dcfrankel <frankelcdavid@gmail.com>
2020-06-24 17:12:37 -0400

    Scroll down to area when dropdown button is clicked

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
-    $('#up').on('click', function () {
+    // Scroll to top button
+    $('#up').on('click', function() {
         $('html, body').animate({
             scrollTop: 0
         }, 2000)
     });
-
+    
+    // Scroll slowly to reference when dropdown clicked
+    $('.dropdown-content a[href*="#"]').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1000)
+    });
+     
+    // Get those cool fade in animations for elements
     AOS.init({
         easing: 'ease',
         duration: 1800,
